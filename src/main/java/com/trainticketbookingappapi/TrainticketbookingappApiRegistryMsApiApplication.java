21ad2a384a02d8325ecc6bc619e775fea8d19381
Devika6795 <deviika1611@gmail.com>
2022-05-05 16:49:31 +0530

    added features

---
+++ src/main/java/com/trainticketbookingappapi/TrainticketbookingappApiRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.trainticketbookingappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainticketbookingappApiRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainticketbookingappApiRegistryMsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainticketbookingappapi/TrainticketbookingappApiRegistryMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainticketbookingappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainticketbookingappApiRegistryMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
